e449bd4f49c7726198682de7aea0af8eeb47c07a
Diogo Padoveis <diogopadoveis@gmail.com>
2024-07-10 13:04:46 -0300

    Creating ChessPosition class

---
+++ ConsoleChess/Entities/Chess/ChessPosition.cs
@@ -0,0 +1,25 @@
+﻿class ChessPosition
+{
+    public char Column { get; set; }
+    public int Line { get; set; }
+
+    public ChessPosition() { }
+
+    public ChessPosition(char column, int line)
+    {
+        Column = column;
+        Line = line;
+    }
+
+    public Position ToPosition()
+    {
+        return new Position(8 - Line, Column - 'a');
+    }
+
+    public override string ToString()
+    {
+        return "" + Line
+                  + Column;
+    }
+}
+
--- ConsoleChess/Program.cs
+++ ConsoleChess/Program.cs
@@ -1,11 +1,9 @@
 ﻿try
 {
-    Board board = new Board(8, 8);
-    board.AddPiece(new Tower(Color.Black, board), new Position(0, 0));
-    board.AddPiece(new Tower(Color.Black, board), new Position(1, 3));
-    board.AddPiece(new King(Color.Black, board), new Position(0, 2));
-    Window.PrintBoard(board);
-    Console.WriteLine();
+    ChessPosition position = new ChessPosition('c',7);
+    Console.WriteLine(position);
+    Console.WriteLine(position.ToPosition());
+
 }
 catch (BoardException e)
 {
