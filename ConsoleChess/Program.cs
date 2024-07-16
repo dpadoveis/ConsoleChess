d9e63f69afced7a0172ff8a6bcad6b6cd32eb33c
Diogo Padoveis <diogopadoveis@gmail.com>
2024-07-16 10:37:35 -0300

    Improving printing of board and pieces

--- ConsoleChess/Program.cs
+++ ConsoleChess/Program.cs
@@ -1,11 +1,18 @@
 ﻿try
 {
-    ChessPosition position = new ChessPosition('c',7);
-    Console.WriteLine(position);
-    Console.WriteLine(position.ToPosition());
+    Board board = new Board(8, 8);
+
+    board.AddPiece(new Tower(Color.Black, board), new Position(0, 0));
+    board.AddPiece(new Tower(Color.Black, board), new Position(1, 3));
+    board.AddPiece(new King(Color.Black, board), new Position(0, 2));
+
+    board.AddPiece(new Tower(Color.White, board), new Position(3, 5));
+
+    Window.PrintBoard(board);
 
 }
 catch (BoardException e)
 {
     Console.WriteLine(e.Message);
 }
+Console.ReadLine();
--- ConsoleChess/UI/Window.cs
+++ ConsoleChess/UI/Window.cs
@@ -2,21 +2,72 @@
 {
     public static void PrintBoard(Board b)
     {
+        ConsoleColor originalBackgroundColor = Console.BackgroundColor;
+        ConsoleColor originalForegroundColor = Console.ForegroundColor;
+
         for (int i = 0; i < b.Lines; i++)
         {
+            Console.Write(8 - i + " ");
             for (int j = 0; j < b.Lines; j++)
-            {
+            {               
+                if ((i + j) % 2 == 0)
+                {
+                    Console.BackgroundColor = ConsoleColor.Gray;
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                }
+                else
+                {
+                    Console.BackgroundColor = ConsoleColor.DarkGreen;
+                    Console.ForegroundColor = ConsoleColor.DarkGreen;
+                }
+
                 if (b.Pieces[i, j] == null)
                 {
                     Console.Write("- ");
                 }
                 else
-                {
-                    Console.Write(b.Pieces[i, j] + " ");
+                {                  
+                    if ((i + j) % 2 == 0)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Black;
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    PrintPiece(b.Pieces[i, j]);
+                    Console.Write(" ");
                 }
+
+                
+                Console.ResetColor();
             }
             Console.WriteLine();
         }
+
+        Console.WriteLine("  a b c d e f g h");
+
+        Console.BackgroundColor = originalBackgroundColor;
+        Console.ForegroundColor = originalForegroundColor;
+    }
+
+    public static void PrintPiece(Piece piece)
+    {
+        if (piece.Color == Color.Black)
+        {
+            ConsoleColor aux = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Write(piece);
+            Console.ForegroundColor = aux;
+        }
+        else
+        {
+            ConsoleColor aux = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write(piece);
+            Console.ForegroundColor = aux;
+        }
+
     }
 }
 
